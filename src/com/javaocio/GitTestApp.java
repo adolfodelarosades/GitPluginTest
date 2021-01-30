1941b13c1b28ea9df566302c1fd09eb4cea97039
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2021-01-30 20:27:42 +0100

    Commit new branch bug#1234

--- src/com/javaocio/GitTestApp.java
+++ src/com/javaocio/GitTestApp.java
@@ -3,7 +3,7 @@ package com.javaocio;
 public class GitTestApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		System.out.println("Hello World, from branch bug#1234 !!!");
 
 	}
 
